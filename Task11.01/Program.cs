b2d1dc3aa081cc93772a373b3148d5dd1cc705ab
andreevadp <destroyapoison@yandex.ru>
2022-10-14 23:19:47 +0300

    Найти третью цифру числа или сообщить, что её нет ЧЕРЕЗ ИНДЕКС

---
+++ Task11.01/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число: ");
+string Number = Console.ReadLine();
+if (Number.Length < 3)
+    Console.WriteLine("Третьей цифры в числе нет");
+else
+{
+    Console.WriteLine($"Третья цифра: {Number[2]}");    
+}    
