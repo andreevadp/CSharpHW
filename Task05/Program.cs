ca5f7c5df45f22a6a823f0e810fb7843d015a062
andreevadp <destroyapoison@yandex.ru>
2022-10-14 21:23:46 +0300

    Показать четные числа от 1 до N

---
+++ Task05/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+for (int n = a; n <= b; n = n + 1)
+{
+    if (n % 2 == 0) 
+    {
+     Console.Write(n + " ");
+    }
+}
