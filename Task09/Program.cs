46305af73e60e530b4c049cb0875bce10efdae59
andreevadp <destroyapoison@yandex.ru>
2022-10-14 21:58:54 +0300

    Удалить вторую цифру трёхзначного числа

---
+++ Task09/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число ");
+int Number = int.Parse(Console.ReadLine());
+
+int a = Number / 100;
+int b = Number % 10;
+
+Console.WriteLine("Заданное число: " + Number);
+Console.WriteLine("Число без второй цифры: " + a + b);
