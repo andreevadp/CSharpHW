f6779de8bb7b6a3f35a01ece93ad87e84629c239
andreevadp <destroyapoison@yandex.ru>
2022-10-14 21:34:46 +0300

    Показать последнюю цифру трёхзначного числа

---
+++ Task06/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int y = a % 10;
+Console.WriteLine("Последняя цифра " + y);
